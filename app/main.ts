d3934007f7d4d173433a9c6c2a96808d41006174
Paul Wooten <pawooten@gmail.com>
2017-04-27 10:26:56 -0400

    Adding routing navigation example

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,19 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }  from './components/app.component';
+import {HomeComponent} from "./components/home";
+import {ProductDetailComponent} from "./components/product";
+import {LocationStrategy, HashLocationStrategy, APP_BASE_HREF} from '@angular/common';
+import {routing} from './components/app.routing';
 
-import { AppModule }  from './app.module';
+@NgModule({
+    imports:      [ BrowserModule, routing ],
+    declarations: [ AppComponent, HomeComponent, ProductDetailComponent],
+    providers:[{provide: LocationStrategy, useClass: HashLocationStrategy}],
+    //providers:[{provide: APP_BASE_HREF, useValue: '/'}], // requires configuring redirects on web server
+    bootstrap:    [ AppComponent ]
+})
+class AppModule { }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
